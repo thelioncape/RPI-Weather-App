739889977491776d5292f46ebc7143eb5efaa7d6
softkittenstudios <softkittenstudios@gmail.com>
2019-01-26 16:38:14 +0000

    First release of main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import urllib.request
 import json
-import time
-# import RPi.GPIO as GPIO
+from time import sleep
+import RPi.GPIO as GPIO
 
 url = "http://api.openweathermap.org/data/2.5/weather?"
 
 
-def round_to_compass_point(number):
-    # Take degrees, devide by 45 and ronud to find direction from table below
-    return round(int(number) / 45)
+def setup_pins():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(10, GPIO.OUT) # North LED
+    GPIO.setup(12, GPIO.OUT) # North-East LED
+    GPIO.setup(16, GPIO.OUT) # East LED
+    GPIO.setup(18, GPIO.OUT) # South-East LED
+    GPIO.setup(22, GPIO.OUT) # South LED
+    GPIO.setup(24, GPIO.OUT) # South-West LED
+    GPIO.setup(26, GPIO.OUT) # West LED
+    GPIO.setup(28, GPIO.OUT) # North-West LED
 
-# 0: North
-# 1: North-East
-# 2: East
-# 3: South-East
-# 4: South
-# 5: South-West
-# 6: West
-# 7: North-West
+
+def round_to_compass_point(number):
+    # Take degrees, devide by 45 and ronud to find direction in number (0 as north)
+    return round(int(number) / 45)
 
 
 def get_weather():
@@ -44,12 +47,53 @@ def get_wind():
     weather = get_weather()
     wind_speed = weather['wind']['speed']
     # Convert meter per second to miles per hour
-    wind_speed = float(weather['wind']['speed'])*2.236936
+    wind_speed = int(int(weather['wind']['speed'])*2.236936)
+    if wind_speed > 100:
+        wind_speed = 100
     wind_direction = round_to_compass_point(weather['wind']['deg'])
     return [wind_speed, wind_direction]
 
 
+def set_wind_direction(direction):
+    GPIO.output(10, GPIO.LOW)  # North
+    GPIO.output(12, GPIO.LOW)  # North-East
+    GPIO.output(16, GPIO.LOW)  # East
+    GPIO.output(18, GPIO.LOW)  # South-East
+    GPIO.output(22, GPIO.LOW)  # South
+    GPIO.output(24, GPIO.LOW)  # South-West
+    GPIO.output(26, GPIO.LOW)  # West
+    GPIO.output(28, GPIO.LOW)  # North-West
+    if direction == 0:
+        GPIO.output(10, GPIO.HIGH)
+    elif direction == 1:
+        GPIO.output(12, GPIO.HIGH)
+    elif direction == 2:
+        GPIO.output(16, GPIO.HIGH)
+    elif direction == 3:
+        GPIO.output(18, GPIO.HIGH)
+    elif direction == 4:
+        GPIO.output(22, GPIO.HIGH)
+    elif direction == 5:
+        GPIO.output(24, GPIO.HIGH)
+    elif direction == 6:
+        GPIO.output(26, GPIO.HIGH)
+    elif direction == 7:
+        GPIO.output(28, GPIO.HIGH)
+
+
+def set_wind_speed(speed):
+    # take speed and divide by 100. Times result by total time in one iteration
+    # one iteration should last 0.01 seconds
+    ontime = (speed / 100) * 0.01
+    offtime = 0.01 - ontime
+    for i in range(5000):
+        GPIO.output(32, GPIO.HIGH)
+        sleep(ontime)
+        GPIO.output(32, GPIO.LOW)
+        sleep(offtime)
+
 while True:
-    print(get_wind())
-    time.sleep(60)
+    wind = get_wind()
+    set_wind_direction(wind[1])
+    set_wind_speed(wind[0])
 
